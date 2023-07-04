277b3afd16783a01574443a33730bc717a350417
esmabecovic <becovicesma@gmail.com>
2023-07-04 15:10:33 +0200

    weather update

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -3,35 +3,38 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import SearchBar from "../products/searchbar";
 
-// const API_KEY = `38841c854f77441f8ff132030230307`
 const API_KEY = `dde669c54dbec53c562abad52d716702`;
 const BASE_URL = `https://api.openweathermap.org/data/2.5`;
 
-const fetchData = async () => {
+const fetchData = async (city) => {
   const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`
+    `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
   );
   const data = response.data;
   return data;
 };
 
-console.log(fetchData());
+console.log(fetchData("London"));
 
 const GetCity = () => {
   const [dataFromSearch, setDataFromSearch] = useState("");
   const { data, isLoading, isError, error } = useQuery("city", fetchData);
+  fetchData(dataFromSearch )
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
+
   if (isError) {
-    return <div>Error:{error.message}</div>;
+    return <div>Error: {error.message}</div>;
   }
+
   const updateSearch = (e) => {
     setDataFromSearch(e);
   };
-  const filterData = data.filter((city) =>
-    city.name.toLowerCase().includes(dataFromSearch.toLowerCase())
-  );
+
+  const filterData =
+    data && data.name.toLowerCase().includes(dataFromSearch.toLowerCase());
 
   return (
     <div className="main">
@@ -39,14 +42,15 @@ const GetCity = () => {
         <SearchBar onText={updateSearch} />
       </div>
       <div className="container">
-        {filterData(city => (
-          <div className="card" key={city.id}>
-            <h1>{city.name}</h1>
-            <img src={city.icon} />
-            <h2>{city.main.temp}</h2>
-            <h2>{city.clouds.all}</h2>
+        {filterData && (
+          <div className="card" key={data.id}>
+            <h1>{data.name}</h1>
+            {/* Replace 'city.icon' with the correct property from the API response */}
+            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} />
+            <h2>{data.main.temp}</h2>
+            <h2>{data.clouds.all}</h2>
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
